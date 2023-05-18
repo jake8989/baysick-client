aa520daa6e40cb8267df9fa02d3c1931ba6aa56b
jake <jjk848798@gmail.com>
2023-05-19 00:51:29 +0530

    .env

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,23 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Nav1 from '@/components/Navbar';
-
+import { makeStyles } from '@material-ui/styles';
+import { useTheme } from '@material-ui/styles';
+import { Height } from '@mui/icons-material';
+const useStyles = makeStyles((theme) => ({
+	section: {
+		height: '100vh',
+		// width: '100vw',
+		background: 'black',
+	},
+}));
 export default function Home() {
+	const classes = useStyles();
+	const theme = useTheme();
 	return (
 		<main>
 			<Nav1></Nav1>
-			<h1>fogherig</h1>
+			<section className={classes.section}></section>
 		</main>
 	);
 }
