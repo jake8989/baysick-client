913d37b3f624d394f4106982dbdb1b41452649bd
jake <jjk848798@gmail.com>
2023-05-22 23:48:12 +0530

    frontend-cure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+	async headers() {
+		return [
+			{
+				// Allow all requests from all origins
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: '*',
+					},
+				],
+			},
+		];
+	},
+};
