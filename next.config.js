55586d3ab28f7d38bacdfad376a3470bb27bbc0a
jake <jjk848798@gmail.com>
2023-05-22 23:59:19 +0530

    .

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-module.exports = {
-	async headers() {
-		return [
-			{
-				// Allow all requests from all origins
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'Access-Control-Allow-Origin',
-						value: '*',
-					},
-				],
-			},
-		];
-	},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
 };
+
+module.exports = nextConfig;
