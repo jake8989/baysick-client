9fb6fa77328ed114b90075808c81e3ce59a6df0e
jake <jjk848798@gmail.com>
2023-07-02 18:07:49 +0530

    added:validation to email and phone

--- hooks/useSignup.js
+++ hooks/useSignup.js
@@ -3,6 +3,7 @@ import { AuthContext } from '@/context/AuthContext';
 import axios from 'axios';
 import { useAuthContext } from './useAuthContext';
 import { ReportSharp } from '@mui/icons-material';
+import { validate } from 'email-validator';
 
 export const useSignup = () => {
 	const [user, setUser] = useState(null);
@@ -10,6 +11,49 @@ export const useSignup = () => {
 	const [loading, setLoading] = useState(false);
 	const [success, setSuccess] = useState(null);
 	const { dispatch } = useAuthContext();
+	let emailApi = '3d02646141384850ad3e7fa3010a31f3';
+	let phoneAPi = '63cdba0046644504968016ab57722134';
+	console.log(emailApi, phoneAPi);
+	const validateEmail = (email, setError) => {
+		axios
+			.get(
+				`https://emailvalidation.abstractapi.com/v1/?api_key=${emailApi}&email=${email}`
+			)
+			.then((response) => {
+				console.log(response.data);
+				if (response.data.deliverability == 'DELIVERABLE') {
+					return true;
+				}
+				// return false;
+			})
+			.catch((error) => {
+				console.log(error);
+				setError('Sorry Email services are not working');
+			});
+	};
+	const validatePhone = (phone, setError) => {
+		phone = '91' + phone;
+		console.log(phone);
+		axios
+			.get(
+				`https://phonevalidation.abstractapi.com/v1/?api_key=${phoneAPi}&phone=${phone}`
+			)
+			.then((response) => {
+				console.log(response.data);
+				if (response.data.valid) {
+					return true;
+				}
+				return false;
+			})
+			.catch((error) => {
+				console.log(error);
+
+				setError('Sorry Phone services are not working');
+
+				// return false;
+			});
+	};
+
 	const signup = async (name, email, phone, password, router) => {
 		setLoading(true);
 		const postData = {
@@ -18,8 +62,24 @@ export const useSignup = () => {
 			phone,
 			password,
 		};
+
+		// return;
+
+		// validatePhone(phone, setError);
+		// setError('Phone is not correct! please provide a valid phone number');
+		// if (!validateEmail(email, setError)) {
+		// 	setError('Email is not correct! please provide a valid email address');
+		// 	setLoading(false);
+		// 	return;
+		// }
+		// if (!validatePhone(phone, setError)) {
+		// 	setError('phone is not correct! please provide a valid phone number');
+		// 	setLoading(false);
+		// 	return;
+		// }
+
 		axios
-			.post(`${process.env.NEXT_PUBLIC_BACKEND}/api/users/login`, postData)
+			.post(`${process.env.NEXT_PUBLIC_BACKEND}/api/users/signup`, postData)
 			.then((response) => {
 				// Handle successful response
 				console.log(response);
@@ -46,45 +106,11 @@ export const useSignup = () => {
 					setError(error.response.data.message);
 					setLoading(false);
 				} else {
-					setError(
-						'Something went wrong! use different mobile number and email to continue'
-					);
+					setError('Something went wrong! Server ErRor');
 					setLoading(false);
 					console.error(error);
 				}
 			});
-		// try {
-		// 	const response = await fetch(
-		// 		`${process.env.NEXT_PUBLIC_BACKEND}/api/users/signup`,
-		// 		{
-		// 			method: 'POST',
-		// 			// mode: 'no-cors',
-		// 			headers: { 'Content-Type': 'application/json' },
-		// 			body: JSON.stringify({ name, email, phone, password }),
-		// 		}
-		// 	);
-
-		// 	const json = await response.json();
-
-		// 	console.log(json);
-		// 	if (!response.ok) {
-		// 		setLoading(false);
-		// 		console.log(json.message);
-		// 		setError(json.message);
-		// 		throw Error('Server not working');
-		// 	}
-		// 	if (response.ok) {
-		// 		localStorage.setItem('user', JSON.stringify(json));
-		// 		dispatch({ type: 'LOGIN', payload: json });
-		// 		setLoading(false);
-		// 		setSuccess('ok');
-		// 		router.push('/');
-		// 	}
-		// } catch (error) {
-		// 	console.log(error.message);
-		// 	setError(error.message);
-		// 	setLoading(false);
-		// }
 	};
 	return { signup, loading, error, setError, success, setSuccess };
 };
